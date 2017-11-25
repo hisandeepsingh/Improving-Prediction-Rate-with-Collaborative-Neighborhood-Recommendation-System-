6aa799d53486302ab5cd2a9d71cd1935f37c9c23
sandeep <hisandeepsingh@hotmail.com>
2017-11-25 17:37:09 +0530

    different algos comparision file

---
+++ testing.py
@@ -0,0 +1,36 @@
+import pandas as pd
+import surprise
+from surprise import SVD
+from surprise import KNNBasic
+from surprise import KNNBaseline
+from surprise import KNNWithMeans
+from surprise import NormalPredictor
+from surprise import Dataset
+from surprise import evaluate, print_perf
+import time
+data = Dataset.load_builtin('ml-100k')
+u_cols = ['user_id', 'age', 'sex', 'occupation', 'zip_code']
+users = pd.read_csv('ml-100k/u.user', sep='|', names=u_cols,
+                    encoding='latin-1')
+#print users.head()
+data.split(n_folds=2)
+algo1 = SVD()
+algo2= KNNBasic()
+algo3=KNNBaseline()
+algo4=KNNWithMeans()
+algo5=NormalPredictor()
+start_time1=time.time()
+perf1 = evaluate(algo1, data, measures=['RMSE', 'MAE'])
+print("--- %s seconds ---" % (time.time() - start_time1))
+start_time2=time.time()
+perf2 = evaluate(algo2, data, measures=['RMSE', 'MAE'])
+print ("...%s seconds..."%(time.time()-start_time2))
+start_time3=time.time()
+perf3 = evaluate(algo3, data, measures=['RMSE', 'MAE'])
+print ("...%s seconds..."%(time.time()-start_time3))
+start_time4=time.time()
+perf4 = evaluate(algo4, data, measures=['RMSE', 'MAE'])
+print ("...%s seconds..."%(time.time()-start_time4))
+start_time5=time.time()
+perf5 = evaluate(algo5, data, measures=['RMSE', 'MAE'])
+print ("...%s seconds..."%(time.time()-start_time5))
