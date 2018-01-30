622c6d58e7e7db03c16264006e95a6db94c2aded
hisandeepsingh <hisandeepsingh@hotmail.com>
2018-01-30 16:00:00 +0100

    adding KNN file for improvement

---
+++ KNN.py
@@ -0,0 +1,24 @@
+import io  # needed because of weird encoding of u.item file
+
+from surprise import KNNBaseline
+from surprise import Dataset
+from surprise import get_dataset_dir
+
+
+def read_item_names():
+    """Read the u.item file from MovieLens 100-k dataset and return two
+    mappings to convert raw ids into movie names and movie names into raw ids.
+    """
+
+    file_name = get_dataset_dir() + '/ml-100k/ml-100k/u.item'
+    rid_to_name = {}
+    name_to_rid = {}
+    with io.open(file_name, 'r', encoding='ISO-8859-1') as f:
+        for line in f:
+            line = line.split('|')
+            rid_to_name[line[0]] = line[1]
+            name_to_rid[line[1]] = line[0]
+
+    return rid_to_name, name_to_rid
+
+
